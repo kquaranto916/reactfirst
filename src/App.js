9d55fc2d5bdb7d114d9d8bade3dda75fa8020f85
kquaranto <kquaranto916@g.rwu.edu>
2022-11-15 15:53:39 -0500

    class work 11/15 tried new drop down

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import logo from './rwulogo.png';
 import './App.css';
-import './myFunction.js'; 
+import React from 'react';
 
 function App() {
+  function show(value) {
+    document.querySelector(".text-box").value = value;
+  }
+  
+  let dropdown = document.querySelector(".dropdown")
+  dropdown.onclick = function() {
+      dropdown.classList.toggle("active")
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -16,26 +24,28 @@ function App() {
         </subheading>
       </header>
       </header>
-    <div class="dropdown">
-      <button onclick="myFunction()" class="dropbtn">Majors</button>
-      <div id = "myDropdown" class="dropdown-content">
-        <a href="#">CM</a>
-        <a href="#">COSMC</a>
-        <a href="#">ENG</a>
-      </div>
-    </div>
 
-    <div class="dropdown2">
-      <button onclick="myFunction()" class="dropbtn2">Minors</button>
-      <div id = "myDropdown" class="dropdown-content2">
-        <a href="#">MATH</a>
-        <a href="#">COSMC</a>
-        <a href="#">MANAGEMENT</a>
-        <a href="#">BUSINESS ANALYTICS</a>
+      <div class="dropdown">
+        <input class="text-box" type="text" placeholder="Select on" readonly></input>
+        <div class="options">
+          <div onclick="show('COMSC')">COMSC</div>
+          <div onclick="show('CM')">CM</div>
+          <div onclick="show('ENG')">ENG</div>
+        </div>
       </div>
-    </div>
 
-  </div>
+      <div class="dropdown2">
+        <button onclick="myFunction()" class="dropbtn2">Minors</button>
+        <div id = "myDropdown" class="dropdown-content2">
+          <a href="#">MATH</a>
+          <a href="#">COSMC</a>
+          <a href="#">MANAGEMENT</a>
+          <a href="#">BUSINESS ANALYTICS</a>
+        </div>
+      </div>
+
+    </div>
   );
 }
+
 export default App;
