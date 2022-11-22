bc13a488767c094c39b71d3a61ecdfae6149bfbf
Cassandra P DeBlois <47582046+Sharmena@users.noreply.github.com>
2022-11-22 10:46:37 -0500

    Add to import function

--- src/importExcel.js
+++ src/importExcel.js
@@ -1,11 +1,13 @@
 //This should be able to import excel files so that they can be read through and create objects from
 //type nmd install read-excel-file if need be
+// for now the function just prints to the command line until later use
+function importExcel(major){
 
 const xlsxFile = require('read-excel-file/node');
 
 var semesterCount = 0;
 var i = 0;
-xlsxFile('./src/Requirements.xlsx')
+xlsxFile('./src/Requirements.xlsx', {sheet: major})
 .then((rows) => {
     rows.forEach((row) => {
       row.forEach((cell) => {
@@ -35,4 +37,29 @@ xlsxFile('./src/Requirements.xlsx')
       });
     });
   }); 
+}
+
+/*cases for later use
+
+var major;
+//Should be value of dropdown
+var dropdownValue = document.getElementByID("myDropdown");
+console.log(dropdownValue.options[dropdownValue.selectedIndex].text);
+
+
+switch(dropdownValue){
+  case 'COMSC BA':
+    major = 'Computer Science BA';
+  case 'COMSC BS':
+    major = 'Computer Science BS';
+  case 'ENGR': 
+    major = 'Engineering'
+  case 'CM':
+    major = 'Construction Management'
+}
+
+importExcel(major);
+
+*/
+
 
