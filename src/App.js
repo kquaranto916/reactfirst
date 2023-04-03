2ba95f6dbf78241397617f4dea0e0c2abbccef92
alliehaberlin <70352156+alliehaberlin@users.noreply.github.com>
2023-04-03 14:53:58 -0400

    controlled components replacing uncontrolled

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import { BrowserRouter as Router, Routes, Route}
 	from 'react-router-dom';
 import Home from './pages';
 import Courses from './pages/courses';
+//save function new elements
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app.js';
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+);
 
 function App() {
 return (
