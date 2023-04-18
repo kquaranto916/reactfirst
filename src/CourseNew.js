528aa47324509c60cc960b7e96b874d1ea078d7a
Rachel Piraino <70352162+rpiraino@users.noreply.github.com>
2023-04-17 20:51:29 -0400

    Add files via upload

---
+++ src/CourseNew.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import logo from './RWUlogo.png';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+import initialData from './initial-data-CSBA';
+import Column from './columns';
+import './Courses.css';
+import { DragDropContext } from 'react-beautiful-dnd';
+
+const Container = styled.div`
+  padding-top: 5px;
+  padding-bottom: 10px;
+  width: 40%;
+  padding-left: 30%;
+  display: inline-block;
+  align-items: center;
+`;
+const Courses1 = () => {
+class App extends React.Component {
+    //This is where we state the initial state from the 4 majors
+    state = initialData;
+
+  onDragEnd = result => {
+    const {destination, source, draggableId} = result;
+  
+    // if the drog location isn't a droppable, don't change anything
+    if (!destination) {
+      return;
+    }
+
+    /// if the drag location is the same droppable and same index, don't change anything
+    if (
+      destination.droppableId === source.droppableId && 
+      destination.index === source.index) {
+      return;
+    }
+
+    const start = this.state.columns[source.droppableId];
+    const finish = this.state.columns[destination.droppableId];
+
+    //if the drag ends in the same droppable, but different index, change the index
+    if(start === finish){
+
+    const newTaskIds = Array.from(start.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...start,
+      taskIds: newTaskIds,
+    };
+ 
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn,
+      },
+    };
+
+    this.setState(newState);
+    return;
+    } 
+
+    //Moving from one droppable to another
+    const startTaskIds = Array.from(start.taskIds);
+    startTaskIds.splice(source.index, 1);
+    const newStart = {
+      ...start,
+      taskIds: startTaskIds,
+    };
+
+    const finishTaskIds = Array.from(finish.taskIds);
+    finishTaskIds.splice(destination.index, 0, draggableId);
+    const newFinish = {
+      ...finish,
+      taskIds: finishTaskIds,
+    };
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newStart.id]: newStart,
+        [newFinish.id]: newFinish,
+      },
+    };
+
+    this.setState(newState);
+  };
+
+  render() {
+    return (
+        <div className="Course">
+        <header className="Course-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>
+            Advising Assistant
+          </h1>
+        </header>
+      <DragDropContext onDragEnd = {this.onDragEnd}>
+        <Container>
+      {this.state.columnOrder.map(columnId => {
+      const column = this.state.columns[columnId];
+      const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+      return <Column key = {column.id} column = {column} tasks = {tasks} />;
+    })}
+    </Container>
+    </DragDropContext>
+    </div>
+    
+    );
+  }
+ }
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+};
+export default Courses1;
